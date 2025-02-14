676f035128c5db1bb86c455de8f43a874739f2d2
Bina <benjaminbinyam500@gmail.com>
2025-02-14 11:28:13 +0300

    components created and data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
-const url = 'https://www.course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+
+const url = "https://www.course-api.com/react-tours-project";
 
 const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
